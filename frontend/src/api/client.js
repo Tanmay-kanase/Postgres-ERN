c2907ac745a244e830b0d5f9774573933261283e
Tanmay Kanase <tanmaykanase06@gmial.com>
2026-09-18 22:03:48 +0530

    change in URI

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000",
+  baseURL: "https://postgres-ern.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
@@ -19,7 +19,7 @@ api.interceptors.response.use(
       window.location.href = "/login";
     }
     return Promise.reject(err);
-  }
+  },
 );
 
 export default api;
